472ef8dc98c88b0200879adc282bfa929274dc1c
daodinhduc1998 <44393386+daodinhduc1998@users.noreply.github.com>
2021-07-01 20:32:48 +0700

    Save

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP loger
 app.use(morgan('tiny'));
 //Template engine
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'))
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.render('home');
+})
+app.get('/news', (req, res) => {
+    res.render('news');
 })
 
 app.listen(port, () => {
