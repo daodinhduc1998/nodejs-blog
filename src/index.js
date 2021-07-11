d62038203d9abcff6775756063e8f3475de52341
daodinhduc1998 <44393386+daodinhduc1998@users.noreply.github.com>
2021-07-11 17:19:39 +0700

    Fix error

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 //HTTP loger
 app.use(morgan('tiny'));
 //Template engine
@@ -21,6 +25,15 @@ app.get('/', (req, res) => {
 })
 app.get('/news', (req, res) => {
     res.render('news');
+})
+
+app.get('/search', (req, res) => {
+    //console.log(req.query)
+    res.render('search');
+})
+app.post('/search', (req, res) => {
+    console.log(req.body)
+    res.send('POST');
 })
 
 app.listen(port, () => {
