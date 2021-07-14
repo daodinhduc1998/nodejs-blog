5dc301a314dfd9a4d328bc4ad13f3c56f6db075b
daodinhduc1998 <44393386+daodinhduc1998@users.noreply.github.com>
2021-07-14 14:50:00 +0700

    add to DB

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -14,5 +14,17 @@ class CourseController {
         res.render('courses/create');
     }
 
+    store(req, res, next) {
+        const formData = req.body;
+        formData.img = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+        const course = new Course(formData);
+        course.save()
+            .then(() => res.redirect('/'))
+            .catch(next);
+
+
+
+    }
+
 }
 module.exports = new CourseController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
-
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
+    name: { type: String, maxLength: 255, require: true },
     des: { type: String, maxLength: 600 },
     img: { type: String, maxLength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+    videoId: { type: String },
+    slug: { type: String, slug: "name", unique: true }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Course', Course, 'Courses');
